9eb343085a26c82a00dbd45ce2e5f485ccd10549
Vechkanova <vechkanova.p@gmail.com>
2021-09-20 19:37:08 +0400

    Добавление класса SqrFunction

--- src/main/java/functions/SqrFunction.java
+++ src/main/java/functions/SqrFunction.java
@@ -1,6 +1,6 @@
 package functions;
 
-public class SqrFunction implements MathFunction{
+public class SqrFunction implements MathFunction {
     @Override
     public double apply(double x) {
         return Math.pow (x,2);
