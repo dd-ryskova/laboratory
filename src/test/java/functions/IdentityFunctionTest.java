8c002d8794d20368a9739868b5225b355834a7e8
Ryskova <ddryskova2002@gmail.com>
2021-09-20 18:22:55 +0400

    Добавление тестового класса

--- src/test/java/functions/IdentityFunctionTest.java
+++ src/test/java/functions/IdentityFunctionTest.java
@@ -1,7 +1,16 @@
 package functions;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.testng.annotations.Test;
+import functions.IdentityFunction;
+import static org.testng.Assert.*;
 
 class IdentityFunctionTest {
 
-}
+        @Test
+        public void testApply() {
+            assertEquals(new IdentityFunction().apply(3.), 3.);
+            assertEquals(new IdentityFunction().apply(25.), 25.);
+            assertEquals(new IdentityFunction().apply(456.), 456.);
+        }
+    }
+
