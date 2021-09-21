e010ef7f00594936aedc90151950ccf32d480ee2
Vechkanova <vechkanova.p@gmail.com>
2021-09-21 19:22:24 +0400

    Добавление класса FourDegreeFunction, реализующего 4 степень

--- src/main/java/functions/FourDegreeFunction.java
+++ src/main/java/functions/FourDegreeFunction.java
@@ -2,7 +2,7 @@ package functions;
 
 public class FourDegreeFunction implements MathFunction {
     @Override
-    public double apply(double x) {
+    public double apply(double x){
         return Math.pow(x, 4);
     }
 }
