17efc492f8cfa43b440ff52013dede2cf9afef06
dasha <ddryskova2002@gmail.ru>
2021-10-15 23:06:52 +0400

    Реализованы MockTabulatedFunction и MockTabulatedFunctionTest

---
+++ src/main/java/ru/ssau/tk/DontCry/laboratory/functions/MockTabulatedFunction.java
@@ -0,0 +1,84 @@
+package ru.ssau.tk.DontCry.laboratory.functions;
+
+public class MockTabulatedFunction extends AbstractTabulatedFunction {
+
+    public final double x0 = 0.5;
+    public final double x1 = 3.9;
+    public final double y0 = 2.3;
+    public final double y1 = 5.4;
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Override
+    public double getX(int index) {
+        if (index == 0) {
+            return this.x0;
+        }
+        return this.x1;
+    }
+
+    @Override
+    public double getY(int index) {
+        if (index == 0) {
+            return this.y0;
+        }
+        return this.y1;
+    }
+
+    @Override
+    public void setY(int index, double value) {
+
+    }
+
+    @Override
+    public int indexOfX(double x) {
+        if (x == this.x0) {
+            return 0;
+        }
+        return 1;
+    }
+
+    @Override
+    public int indexOfY(double y) {
+        if (y == this.y0) {
+            return 0;
+        }
+        return 1;
+    }
+
+    @Override
+    public double leftBound() {
+        return this.x0;
+    }
+
+    @Override
+    public double rightBound() {
+        return this.x1;
+    }
+
+    @Override
+    public int floorIndexOfX(double x) {
+        if (x > x1) {
+            return getCount();
+        }
+        return 0;
+    }
+
+    @Override
+    public double extrapolateLeft(double x) {
+        return interpolate(x, 0);
+    }
+
+    @Override
+    public double extrapolateRight(double x) {
+        return interpolate(x, 1);
+    }
+
+    @Override
+    public double interpolate(double x, int floorIndex) {
+        return super.interpolate(x, x0, x1, y0, y1);
+    }
+}
---
+++ src/test/java/ru/ssau/tk/DontCry/laboratory/functions/MockTabulatedFunctionTest.java
@@ -0,0 +1,32 @@
+package ru.ssau.tk.DontCry.laboratory.functions;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class MockTabulatedFunctionTest {
+
+    private static final double DELTA = 0.0001;
+
+    MockTabulatedFunction function = new MockTabulatedFunction();
+
+    @Test
+    public void testInterpolate() {
+        assertEquals(function.interpolate(2.5, 0.5, 3.5, 1.0, 2.0), 1.6666, DELTA);
+        assertEquals(function.interpolate(3.6, -2.4, 4.2, -1.1, 5.7), 5.0818, DELTA);
+        assertEquals(function.interpolate(-1.4, -3.7, 7.6, -5.9, 8.6), -2.9486, DELTA);
+    }
+
+    @Test
+    public void testApply() {
+        assertEquals(function.apply(2.9), 5.4, DELTA);
+        assertEquals(function.apply(5.7), 7.0411, DELTA);
+        assertEquals(function.apply(-4.6), -2.35, DELTA);
+    }
+
+    @AfterMethod
+    void afterMethod() {
+        System.out.println("MockTabulatedFunctionTest checked");
+    }
+}
