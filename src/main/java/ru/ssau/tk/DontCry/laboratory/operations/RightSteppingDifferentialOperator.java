f8bf936ade482ea8e59dd9edfe706e7bc01903da
Vechkanova <vechkanova.p@gmail.com>
2021-10-18 14:41:28 +0400

    Реализованы классы LeftSteppingDifferentialOperator, RightSteppingDifferentialOperator, MiddleSteppingDifferentialOperator

---
+++ src/main/java/ru/ssau/tk/DontCry/laboratory/operations/RightSteppingDifferentialOperator.java
@@ -0,0 +1,15 @@
+package ru.ssau.tk.DontCry.laboratory.operations;
+
+import ru.ssau.tk.DontCry.laboratory.functions.MathFunction;
+
+public class RightSteppingDifferentialOperator extends SteppingDifferentialOperator {
+
+    public RightSteppingDifferentialOperator(double step) {
+        super(step);
+    }
+
+    @Override
+    public MathFunction derive(MathFunction function) {
+        return x -> (function.apply(x + step) - function.apply(x)) / step;
+    }
+}
