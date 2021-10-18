011ac5fbf560ffa9c71be457c1442cf4f3fcf521
Vechkanova <vechkanova.p@gmail.com>
2021-10-18 12:58:13 +0400

    Создано исключение InconsistentFunctionsException

---
+++ src/main/java/ru/ssau/tk/DontCry/laboratory/exceptions/InconsistentFunctionsException.java
@@ -0,0 +1,10 @@
+package ru.ssau.tk.DontCry.laboratory.exceptions;
+
+public class InconsistentFunctionsException extends RuntimeException {
+    public InconsistentFunctionsException() {
+    }
+
+    public InconsistentFunctionsException(String message) {
+        super(message);
+    }
+}
