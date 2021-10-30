35f8979f375a1f4167432819397bdc95efb8c351
dasha <ddryskova2002@gmail.ru>
2021-10-31 00:12:59 +0400

    Добавлен метод статический метод writeTabulatedFunction в классе FunctionsIO

--- src/main/java/ru/ssau/tk/DontCry/laboratory/io/FunctionsIO.java
+++ src/main/java/ru/ssau/tk/DontCry/laboratory/io/FunctionsIO.java
@@ -1,7 +1,23 @@
 package ru.ssau.tk.DontCry.laboratory.io;
 
+import ru.ssau.tk.DontCry.laboratory.functions.Point;
+import ru.ssau.tk.DontCry.laboratory.functions.TabulatedFunction;
+
+import java.io.*;
+
 final public class FunctionsIO {
+
     private FunctionsIO() {
         throw new UnsupportedOperationException();
+    }
+
+    public static void writeTabulatedFunction(BufferedOutputStream outputStream, TabulatedFunction function) throws IOException {
+        DataOutputStream out = new DataOutputStream(outputStream);
+        out.writeInt(function.getCount());
+        for (Point point : function) {
+            out.writeDouble(point.x);
+            out.writeDouble(point.y);
+        }
+        out.flush();
     }
 }
