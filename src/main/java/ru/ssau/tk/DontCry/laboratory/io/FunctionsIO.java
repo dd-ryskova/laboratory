ec42ba5427fd641768ab8426ffe8e5f5b598c35d
Vechkanova <vechkanova.p@gmail.com>
2021-10-31 00:01:11 +0400

    добавлен метод в FunctionsIO

--- src/main/java/ru/ssau/tk/DontCry/laboratory/io/FunctionsIO.java
+++ src/main/java/ru/ssau/tk/DontCry/laboratory/io/FunctionsIO.java
@@ -2,9 +2,15 @@ package ru.ssau.tk.DontCry.laboratory.io;
 
 import ru.ssau.tk.DontCry.laboratory.functions.Point;
 import ru.ssau.tk.DontCry.laboratory.functions.TabulatedFunction;
+import ru.ssau.tk.DontCry.laboratory.functions.factory.TabulatedFunctionFactory;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
 
 final public class FunctionsIO {
     private FunctionsIO() {
@@ -18,5 +24,22 @@ final public class FunctionsIO {
             printWriter.printf("%f %f\n", temp.x, temp.y);
         }
         printWriter.flush();
+    }
+
+    public static TabulatedFunction readTabulatedFunction(BufferedReader reader, TabulatedFunctionFactory factory) throws IOException {
+        int count = Integer.parseInt(reader.readLine());
+        double[] xValues = new double[count];
+        double[] yValues = new double[count];
+        NumberFormat formatter = NumberFormat.getInstance(Locale.forLanguageTag("ru"));
+        for (int i = 0; i < count; i++) {
+            String tempString = reader.readLine();
+            try {
+                xValues[i] = formatter.parse(tempString.split(" ")[0]).doubleValue();
+                yValues[i] = formatter.parse(tempString.split(" ")[1]).doubleValue();
+            } catch (ParseException parseException) {
+                throw new IOException(parseException);
+            }
+        }
+        return factory.create(xValues, yValues);
     }
 }
