d5c100f9f36de69f85650db1a838629e2b5391df
Vechkanova <vechkanova.p@gmail.com>
2021-10-30 23:54:27 +0400

    Создан класс TabulatedFunctionFileWriter и добавлен метод в FunctionsIO

--- src/main/java/ru/ssau/tk/DontCry/laboratory/io/FunctionsIO.java
+++ src/main/java/ru/ssau/tk/DontCry/laboratory/io/FunctionsIO.java
@@ -1,7 +1,22 @@
 package ru.ssau.tk.DontCry.laboratory.io;
 
+import ru.ssau.tk.DontCry.laboratory.functions.Point;
+import ru.ssau.tk.DontCry.laboratory.functions.TabulatedFunction;
+
+import java.io.BufferedWriter;
+import java.io.PrintWriter;
+
 final public class FunctionsIO {
     private FunctionsIO() {
         throw new UnsupportedOperationException();
+    }
+
+    public static void writeTabulatedFunction(BufferedWriter writer, TabulatedFunction function) {
+        PrintWriter printWriter = new PrintWriter(writer);
+        printWriter.println(function.getCount());
+        for (Point temp : function) {
+            printWriter.printf("%f %f\n", temp.x, temp.y);
+        }
+        printWriter.flush();
     }
 }
