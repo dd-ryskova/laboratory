a8313486f99c8719f3bd284062e37126a5dc48de
Vechkanova <vechkanova.p@gmail.com>
2021-10-31 23:57:11 +0400

    Добавлен мето serialize()

--- src/main/java/ru/ssau/tk/DontCry/laboratory/io/FunctionsIO.java
+++ src/main/java/ru/ssau/tk/DontCry/laboratory/io/FunctionsIO.java
@@ -61,4 +61,10 @@ final public class FunctionsIO {
         }
         return factory.create(xValues, yValues);
     }
+
+    public static void serialize(BufferedOutputStream stream, TabulatedFunction function) throws IOException {
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(stream);
+        objectOutputStream.writeObject(function);
+        objectOutputStream.flush();
+    }
 }
