3b4b270a13058af7b6821c155047c372a134c1b1
dasha <ddryskova2002@gmail.ru>
2021-11-12 21:58:19 +0400

    Исправление класса ReadWriteTask

--- src/main/java/ru/ssau/tk/DontCry/laboratory/concurrent/ReadWriteTask.java
+++ src/main/java/ru/ssau/tk/DontCry/laboratory/concurrent/ReadWriteTask.java
@@ -16,11 +16,15 @@ public class ReadWriteTask implements Runnable {
         double y;
         for (int i = 0; i < tabulatedFunction.getCount(); ++i) {
             x = tabulatedFunction.getX(i);
-            y = tabulatedFunction.getY(i);
-            System.out.printf("%s, before write: i = %d, x = %f, y = %f", tabulatedFunction.getClass(), i, x, y);
-            tabulatedFunction.setY(i, y + 1);
-            y = tabulatedFunction.getY(i);
-            System.out.printf("%s, after write: i = %d, x = %f, y = %f", tabulatedFunction.getClass(), i, x, y);
+            synchronized (tabulatedFunction) {
+                y = tabulatedFunction.getY(i);
+                System.out.printf("%s, before write: i =  % d  , x = %f, y = %f", Thread.currentThread().getName(), i, x, y);
+                System.out.println();
+                tabulatedFunction.setY(i, y + 1);
+                y = tabulatedFunction.getY(i);
+            }
+            System.out.printf("%s, after write: i = %d, x = %f, y = %f", Thread.currentThread().getName(), i, x, y);
+            System.out.println();
         }
     }
 }
