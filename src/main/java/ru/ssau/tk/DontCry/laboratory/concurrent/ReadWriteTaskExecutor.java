bce188b256b25fc1cc414e86dae7691f5fc67282
dasha <ddryskova2002@gmail.ru>
2021-11-12 21:51:52 +0400

    Создание класса ReadWriteTaskExecutor

---
+++ src/main/java/ru/ssau/tk/DontCry/laboratory/concurrent/ReadWriteTaskExecutor.java
@@ -0,0 +1,25 @@
+package ru.ssau.tk.DontCry.laboratory.concurrent;
+
+import ru.ssau.tk.DontCry.laboratory.functions.*;
+
+import java.util.ArrayList;
+
+public class ReadWriteTaskExecutor {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        LinkedListTabulatedFunction linkedListTabulatedFunction = new LinkedListTabulatedFunction(new ZeroFunction(), 1, 10, 10);
+        ArrayList<Thread> listThread = new ArrayList<>();
+
+        for (int i = 0; i != 20; ++i) {
+            listThread.add(new Thread(new ReadWriteTask(linkedListTabulatedFunction)));
+        }
+
+        for (Thread thread : listThread) {
+            thread.start();
+        }
+
+        Thread.sleep(2000);
+        System.out.println(linkedListTabulatedFunction.toString());
+    }
+}
