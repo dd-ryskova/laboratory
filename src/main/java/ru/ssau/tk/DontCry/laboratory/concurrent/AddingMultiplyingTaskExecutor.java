0545ad939813833dfb8b2e28f477ce5b2392e4e9
dasha <ddryskova2002@gmail.ru>
2021-12-10 23:03:43 +0400

    Создание MyTableModel

--- src/main/java/ru/ssau/tk/DontCry/laboratory/concurrent/AddingMultiplyingTaskExecutor.java
+++ src/main/java/ru/ssau/tk/DontCry/laboratory/concurrent/AddingMultiplyingTaskExecutor.java
@@ -4,6 +4,7 @@ import ru.ssau.tk.DontCry.laboratory.functions.*;
 
 public class AddingMultiplyingTaskExecutor {
     public static void main(String[] args) throws InterruptedException {
+
         LinkedListTabulatedFunction linkedListTabulatedFunction = new LinkedListTabulatedFunction(new ConstantFunction(2),1,100,100);
         Thread thread1 = new Thread(new MultiplyingTask(linkedListTabulatedFunction));
         Thread thread2 = new Thread(new MultiplyingTask(linkedListTabulatedFunction));
